5ff166b0d02c4dc9e21613b28dde2306f3ed7644
Petr Ospalý <pospaly@opennebula.io>
2020-11-19 13:30:41 +0100

    Replace obsoleted registry basic auth
    Replace the builtin registry's basic auth with the apache's basic auth
    functioning as a proxy pass.
    
    Signed-off-by: Petr Ospalý <pospaly@opennebula.io>

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,26 @@ set -e
 
 # setup basic auth credentials
 if ! [ -f "${REGISTRY_AUTH_HTPASSWD_PATH}" ] ; then
+    echo "Setup the regular registry user: ${REGISTRY_BASIC_AUTH_USERNAME}"
     htpasswd -Bbn \
-        "${BASIC_AUTH_USERNAME}" "${BASIC_AUTH_PASSWORD}" \
+        "${REGISTRY_BASIC_AUTH_USERNAME}" \
+        "${REGISTRY_BASIC_AUTH_PASSWORD}" \
         > "${REGISTRY_AUTH_HTPASSWD_PATH}"
+
+    echo "Setup the privileged registry user: ${REGISTRY_BASIC_AUTH_PRIVILEGED_USERNAME}"
+    htpasswd -Bbn \
+        "${REGISTRY_BASIC_AUTH_PRIVILEGED_USERNAME}" \
+        "${REGISTRY_BASIC_AUTH_PRIVILEGED_PASSWORD}" \
+        >> "${REGISTRY_AUTH_HTPASSWD_PATH}"
+fi
+
+# setup privileged group
+if ! [ -f "${REGISTRY_AUTH_HTPASSWD_GROUPS_PATH}" ] ; then
+    echo "Setup the privileged registry group: ${REGISTRY_AUTH_HTPASSWD_PRIVILEGED_GROUP}"
+    printf "%s: %s\n" \
+        "${REGISTRY_AUTH_HTPASSWD_PRIVILEGED_GROUP}" \
+        "${REGISTRY_BASIC_AUTH_PRIVILEGED_USERNAME}" \
+        > "${REGISTRY_AUTH_HTPASSWD_GROUPS_PATH}"
 fi
 
 exit 0
